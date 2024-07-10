3d13f54661f68ddbfabd23dbf83134ffe44efd4e
unknown <rahmahsaif6@gmail.com>
2024-07-10 11:13:53 +0400

    commit branch2 created

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ app.use(express.static(path.join(__dirname, 'build')))
 app.get('/*', function(req,res){
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
   });
-  const PORT = process.env.PORT || 5000;
-  app.listen(PORT, () => {
+  // yes, branch2 is here!!
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
     console.log(`Server is running on PORT ${PORT}`);
   });  
